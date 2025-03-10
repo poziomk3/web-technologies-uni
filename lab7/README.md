6ccdad218378155144da50ac7a79f5754a658282
Karol poziomk3 Zając <karolzajac.0407@gmail.com>
2025-03-10 21:30:32 +0100

    feat: complete task

---
+++ lab7/README.md
@@ -0,0 +1,19 @@
+## standardowo node i npm potrzebny
+
+```
+npm i
+
+```
+
+```
+npm run task1
+```
+
+```
+docker-compose up -d
+ npm run task2
+ ```
+
+```
+http://localhost:4000/graphql
+```
---
+++ lab7/task1/index.js
@@ -0,0 +1,24 @@
+import { createYoga } from "graphql-yoga";
+import { createServer } from "http";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { readFileSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import resolvers from "./resolvers.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const typeDefs = readFileSync(
+  path.join(__dirname, "../schema.graphql"),
+  "utf-8"
+);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+const yoga = createYoga({ schema });
+const server = createServer(yoga);
+
+server.listen(4000, () => {
+  console.log("✅ Server is running at http://localhost:4000/graphql");
+});
