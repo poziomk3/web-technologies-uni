4eaa9ad761fcfa1c3d5c9fcef95adcda7f7e89a8
Karol poziomk3 Zając <karolzajac.0407@gmail.com>
2025-03-28 11:32:27 +0100

    chore: initialize spring repo

---
+++ lab5-spring/src/main/java/com/kd/lab5_spring/Lab5SpringApplication.java
@@ -0,0 +1,13 @@
+package com.kd.lab5_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab5SpringApplication.class, args);
+	}
+
+}
